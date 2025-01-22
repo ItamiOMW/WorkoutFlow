3ceaefd56eeff0bd91d9a46e32d4469fe46a7220
ItamiOMW <itamiomw@gmail.com>
2025-01-22 12:35:27 +0300

    Add main navigation graphs

--- composeApp/src/commonMain/kotlin/com/itami/workout_flow/core/presentation/navigation/AppGraph.kt
+++ composeApp/src/commonMain/kotlin/com/itami/workout_flow/core/presentation/navigation/AppGraph.kt
@@ -1,5 +1,7 @@
 package com.itami.workout_flow.core.presentation.navigation
 
+import com.itami.workout_flow.model.WorkoutType
+import com.itami.workout_flow.model.WorkoutsSort
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -15,6 +17,34 @@ sealed interface AppGraph {
     data object Home : AppGraph {
         @Serializable
         data object HomeScreen
+    }
+
+    @Serializable
+    data object Workouts : AppGraph {
+        @Serializable
+        data class WorkoutsScreen(
+            val launchMode: WorkoutsLaunchMode = WorkoutsLaunchMode.Default,
+            val workoutType: WorkoutType? = null,
+            val workoutSort: WorkoutsSort = WorkoutsSort.Newest,
+        ) {
+            enum class WorkoutsLaunchMode {
+                Default,
+                Search,
+                Favorites
+            }
+        }
+    }
+
+    @Serializable
+    data object Progress : AppGraph {
+        @Serializable
+        data object ProgressScreen
+    }
+
+    @Serializable
+    data object Profile : AppGraph {
+        @Serializable
+        data object CurrentUserProfileScreen
     }
 
 }
