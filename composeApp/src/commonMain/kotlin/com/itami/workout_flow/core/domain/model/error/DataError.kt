9749bac05e325b6ed78d4fbeb22d8224f379a276
ItamiOMW <itamiomw@gmail.com>
2025-02-10 12:38:24 +0300

    Add UNAUTHORIZED and NOT_FOUND remote data errors

--- composeApp/src/commonMain/kotlin/com/itami/workout_flow/core/domain/model/error/DataError.kt
+++ composeApp/src/commonMain/kotlin/com/itami/workout_flow/core/domain/model/error/DataError.kt
@@ -3,6 +3,8 @@ package com.itami.workout_flow.core.domain.model.error
 sealed interface DataError : AppError {
 
     enum class Remote : DataError {
+        UNAUTHORIZED,
+        NOT_FOUND,
         REQUEST_TIMEOUT,
         TOO_MANY_REQUESTS,
         NO_INTERNET,
