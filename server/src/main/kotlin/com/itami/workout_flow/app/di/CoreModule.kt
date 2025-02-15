b333bd04207dc2749ad113529a85a3a7ad0a5df5
ItamiOMW <itamiomw@gmail.com>
2025-02-15 09:43:36 +0300

    AuthService injection

--- server/src/main/kotlin/com/itami/workout_flow/app/di/CoreModule.kt
+++ server/src/main/kotlin/com/itami/workout_flow/app/di/CoreModule.kt
@@ -2,10 +2,12 @@ package com.itami.workout_flow.app.di
 
 import com.itami.workout_flow.data.repository.user.UserRepository
 import com.itami.workout_flow.data.repository.user.UserRepositoryImpl
+import com.itami.workout_flow.service.auth.AuthService
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreModule = module {
+    singleOf(::AuthService)
     singleOf(::UserRepositoryImpl).bind<UserRepository>()
 }
