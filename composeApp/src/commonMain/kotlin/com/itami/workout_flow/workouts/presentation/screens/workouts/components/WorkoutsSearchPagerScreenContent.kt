2ad07a779375ea0334e6bbcd7fbba114605821bb
ItamiOMW <itamiomw@gmail.com>
2025-03-08 08:10:10 +0300

    Add custom implementation for LazyPagingItems

--- composeApp/src/commonMain/kotlin/com/itami/workout_flow/workouts/presentation/screens/workouts/components/WorkoutsSearchPagerScreenContent.kt
+++ composeApp/src/commonMain/kotlin/com/itami/workout_flow/workouts/presentation/screens/workouts/components/WorkoutsSearchPagerScreenContent.kt
@@ -19,20 +19,21 @@ import androidx.compose.ui.unit.dp
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.paging.PagingData
-import app.cash.paging.compose.collectAsLazyPagingItems
-import app.cash.paging.compose.itemContentType
-import app.cash.paging.compose.itemKey
-import com.itami.workout_flow.core.domain.model.error.AppError
 import com.itami.workout_flow.core.domain.model.error.DataError
 import com.itami.workout_flow.core.domain.model.workout.WorkoutPreview
 import com.itami.workout_flow.core.presentation.components.ShimmerWorkoutItem
 import com.itami.workout_flow.core.presentation.components.WorkoutPreviewItem
 import com.itami.workout_flow.core.presentation.theme.WorkoutFlowTheme
 import com.itami.workout_flow.core.presentation.utils.toStringRes
+import com.itami.workout_flow.core.presentation.utils.LazyPagingItems
+import com.itami.workout_flow.core.presentation.utils.itemKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import org.jetbrains.compose.resources.getString
 import workoutflow.composeapp.generated.resources.Res
 import workoutflow.composeapp.generated.resources.error_unknown
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 @Composable
 internal fun WorkoutsSearchPagerScreenContent(
@@ -42,7 +43,7 @@ internal fun WorkoutsSearchPagerScreenContent(
     onWorkoutPreviewClick: (WorkoutPreview) -> Unit,
     onErrorOccurred: (m: String) -> Unit,
 ) {
-    val searchWorkoutsPagingItems = searchWorkoutsPreviewsPagingItemsFlow.collectAsLazyPagingItems()
+    val searchWorkoutsPagingItems = searchWorkoutsPreviewsPagingItemsFlow.collectAsLazyPagingItems1()
 
     // State to track the last displayed error and its handling status
     var lastDisplayedError by remember { mutableStateOf<String?>(null) }
@@ -72,7 +73,6 @@ internal fun WorkoutsSearchPagerScreenContent(
             items(
                 count = searchWorkoutsPagingItems.itemCount,
                 key = searchWorkoutsPagingItems.itemKey { it.id },
-                contentType = searchWorkoutsPagingItems.itemContentType { "SearchWorkoutsPagingItems" }
             ) { index ->
                 val workoutPreview = searchWorkoutsPagingItems[index]
                 if (workoutPreview != null) {
@@ -113,6 +113,35 @@ internal fun WorkoutsSearchPagerScreenContent(
     }
 }
 
+@Composable
+fun <T : Any> Flow<PagingData<T>>.collectAsLazyPagingItems1(
+    context: CoroutineContext = EmptyCoroutineContext
+): LazyPagingItems<T> {
+
+    val lazyPagingItems = remember(this) { LazyPagingItems(this) }
+
+    LaunchedEffect(lazyPagingItems) {
+        if (context == EmptyCoroutineContext) {
+            lazyPagingItems.collectPagingData()
+        } else {
+            withContext(context) {
+                lazyPagingItems.collectPagingData()
+            }
+        }
+    }
+
+    LaunchedEffect(lazyPagingItems) {
+        if (context == EmptyCoroutineContext) {
+            lazyPagingItems.collectLoadState()
+        } else {
+            withContext(context) {
+                lazyPagingItems.collectLoadState()
+            }
+        }
+    }
+
+    return lazyPagingItems
+}
 
 private suspend fun getCurrentErrorMessage(loadState: CombinedLoadStates): String? {
     return when {
@@ -120,10 +149,12 @@ private suspend fun getCurrentErrorMessage(loadState: CombinedLoadStates): Strin
             val error = (loadState.refresh as LoadState.Error).error
             getErrorMessage(error)
         }
+
         loadState.append is LoadState.Error -> {
             val error = (loadState.append as LoadState.Error).error
             getErrorMessage(error)
         }
+
         else -> null // No error in the current load state
     }
 }
@@ -135,6 +166,7 @@ private suspend fun getErrorMessage(error: Throwable): String {
             val errorMessage = getString(errorMessageStringRes)
             errorMessage
         }
+
         else -> getString(Res.string.error_unknown)
     }
 }
