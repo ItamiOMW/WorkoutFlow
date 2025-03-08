caf46d1f206bd20caba25f61c0d4c9a6eefd80d4
ItamiOMW <itamiomw@gmail.com>
2025-03-08 08:16:58 +0300

    Implement EquipmentExt

--- composeApp/src/commonMain/kotlin/com/itami/workout_flow/core/presentation/utils/EquipmentExt.kt
+++ composeApp/src/commonMain/kotlin/com/itami/workout_flow/core/presentation/utils/EquipmentExt.kt
@@ -5,7 +5,11 @@ import com.itami.workout_flow.model.Equipment
 import com.itami.workout_flow.model.Muscle
 import org.jetbrains.compose.resources.stringResource
 import workoutflow.composeapp.generated.resources.Res
+import workoutflow.composeapp.generated.resources.bands
+import workoutflow.composeapp.generated.resources.barbell
 import workoutflow.composeapp.generated.resources.biceps
+import workoutflow.composeapp.generated.resources.bodyweight
+import workoutflow.composeapp.generated.resources.calisthenics
 import workoutflow.composeapp.generated.resources.chest
 import workoutflow.composeapp.generated.resources.core
 import workoutflow.composeapp.generated.resources.forearms
@@ -22,15 +26,17 @@ import workoutflow.composeapp.generated.resources.triceps
 import workoutflow.composeapp.generated.resources.upper_traps
 import workoutflow.composeapp.generated.resources.neck
 import workoutflow.composeapp.generated.resources.calves
+import workoutflow.composeapp.generated.resources.dumbbells
+import workoutflow.composeapp.generated.resources.machines
 
 @Composable
 fun Equipment.getName(): String {
     return when (this) {
-        Equipment.Bodyweight -> stringResource(Res.string.chest)
-        Equipment.Calisthenics -> TODO()
-        Equipment.Barbell -> TODO()
-        Equipment.Dumbbells -> TODO()
-        Equipment.Machines -> TODO()
-        Equipment.Bands -> TODO()
+        Equipment.Bodyweight -> stringResource(Res.string.bodyweight)
+        Equipment.Calisthenics -> stringResource(Res.string.calisthenics)
+        Equipment.Barbell -> stringResource(Res.string.barbell)
+        Equipment.Dumbbells -> stringResource(Res.string.dumbbells)
+        Equipment.Machines -> stringResource(Res.string.machines)
+        Equipment.Bands -> stringResource(Res.string.bands)
     }
 }
