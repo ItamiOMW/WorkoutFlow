a7de37a02ed6c76bd2c681b84ca16d90fecb084b
ItamiOMW <itamiomw@gmail.com>
2025-03-11 12:13:07 +0300

    Remove unused imports

--- server/src/main/kotlin/com/itami/workout_flow/app/Application.kt
+++ server/src/main/kotlin/com/itami/workout_flow/app/Application.kt
@@ -9,9 +9,7 @@ import com.itami.workout_flow.app.plugins.configureSerialization
 import com.itami.workout_flow.app.plugins.configureStatusPages
 import com.itami.workout_flow.data.database.DatabaseFactory
 import com.itami.workout_flow.data.firebase.FirebaseFactory
-import com.itami.workout_flow.data.repository.user.UserRepository
 import io.ktor.server.application.Application
-import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
